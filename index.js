53b1b70b42edef39f84f9dd246735d1f008c7e07
xander-bennett <xanderwbennett@gmail.com>
2023-03-15 21:21:51 -0600

    starter main function for pinging the API

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main() {
+
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await users.json();
+    usersData.map(() =>
+    `<div class="user-card">
+        <div class="user-card__container">
+            <h3>User's Name</h4>
+            <p><b>Email:</b> email@email.com</p>
+            <p><b>Phone:</b> 0000000000</p>
+            <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+        </div>
+        </div>`)
+        .join("")
+        ;
+}
+
+main();
