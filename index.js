923c26dba0db87edd0d9cfa835009f3e04b46deb
xander-bennett <xanderwbennett@gmail.com>
2023-03-16 23:47:32 -0600

    added async/await notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
+
+// 1. Fetch the users from the API
 async function main() {
 
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -19,3 +21,47 @@ async function main() {
 }
 
 main();
+
+
+/*
+
+Here's how async/await works:
+
+The async keyword is used to define a function as asynchronous. 
+This means that the function will always return a Promise. 
+You can use the await keyword inside an async function to wait 
+for a Promise to resolve before continuing execution.
+
+When you call an async function, it returns a Promise. 
+You can then use the await keyword to wait for the Promise 
+to resolve before continuing execution.
+
+Here's an example of an async function:
+
+async function myAsyncFunction() {
+  const result = await myPromiseFunction();
+  console.log(result);
+}
+
+function myPromiseFunction() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 2000);
+  });
+}
+
+myAsyncFunction();
+
+
+
+In this example, myAsyncFunction is defined as an async function, 
+and it calls myPromiseFunction using the await keyword. 
+myPromiseFunction returns a Promise that resolves after 2 seconds, 
+and when it resolves, the result is logged to the console.
+
+The advantage of using async/await is that it makes asynchronous code 
+easier to read and write. Instead of chaining multiple .then() calls, 
+you can write code that looks like synchronous code, but still 
+performs asynchronous operations.
+*/
